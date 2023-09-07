0b1b0d008131255044f35b6c9d5c4b94e1fba9d8
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-09-07 21:47:32 +0200

    added responsive style on global css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,21 @@ import styles from "./homepage.module.css";
 export default function Home() {
   return (
     <div>
-      <Link href="/">Hello</Link>
+      <Link href="/">
+        Lorem ipsum dolor sit amet. 33 dolores rerum est tempora galisum a iste
+        provident qui consequatur corrupti. Et commodi quisquam aut fugiat quod
+        id cumque odio qui earum voluptas. Et fugiat ratione nam quod molestiae
+        id omnis veritatis quo quasi minus aut possimus quos est inventore
+        quas.Lorem ipsum dolor sit amet. 33 dolores rerum est tempora galisum a
+        iste provident qui consequatur corrupti. Et commodi quisquam aut fugiat
+        quod id cumque odio qui earum voluptas. Et fugiat ratione nam quod
+        molestiae id omnis veritatis quo quasi minus aut possimus quos est
+        inventore quas.Lorem ipsum dolor sit amet. 33 dolores rerum est tempora
+        galisum a iste provident qui consequatur corrupti. Et commodi quisquam
+        aut fugiat quod id cumque odio qui earum voluptas. Et fugiat ratione nam
+        quod molestiae id omnis veritatis quo quasi minus aut possimus quos est
+        inventore quas.
+      </Link>
     </div>
   );
 }
