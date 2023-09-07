f08df1fe1c4c114165c735996970ef7374ea1073
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-09-07 22:18:09 +0200

    added all folder

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,13 @@
+import { Footer } from "./components/footer/Footer";
+import { Navbar } from "./components/navbar/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Blog App",
-  description: "The best blog app!",
+  title: "akamateus",
+  description: "Tech blog for juniors",
 };
 
 export default function RootLayout({ children }) {
@@ -13,7 +15,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <div className="container">
-          <div className="wrapper">{children}</div>
+          <div className="wrapper">
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
         </div>
       </body>
     </html>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,24 +1,19 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
+import { Featured } from "./components/featured/Featured";
+import { CategoryList } from "./components/category-list/CategoryList";
+import { CardList } from "./components/card-list/CardList";
+import { Menu } from "./components/menu/Menu";
 
 export default function Home() {
   return (
-    <div>
-      <Link href="/">
-        Lorem ipsum dolor sit amet. 33 dolores rerum est tempora galisum a iste
-        provident qui consequatur corrupti. Et commodi quisquam aut fugiat quod
-        id cumque odio qui earum voluptas. Et fugiat ratione nam quod molestiae
-        id omnis veritatis quo quasi minus aut possimus quos est inventore
-        quas.Lorem ipsum dolor sit amet. 33 dolores rerum est tempora galisum a
-        iste provident qui consequatur corrupti. Et commodi quisquam aut fugiat
-        quod id cumque odio qui earum voluptas. Et fugiat ratione nam quod
-        molestiae id omnis veritatis quo quasi minus aut possimus quos est
-        inventore quas.Lorem ipsum dolor sit amet. 33 dolores rerum est tempora
-        galisum a iste provident qui consequatur corrupti. Et commodi quisquam
-        aut fugiat quod id cumque odio qui earum voluptas. Et fugiat ratione nam
-        quod molestiae id omnis veritatis quo quasi minus aut possimus quos est
-        inventore quas.
-      </Link>
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+      <div className={styles.content}>
+        <CardList />
+        <Menu />
+      </div>
     </div>
   );
 }
