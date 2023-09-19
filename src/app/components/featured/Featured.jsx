d54b0ad5576171afffef2e88b0cfc44130543f73
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-09-19 18:21:17 +0200

    added featured page with image

--- src/app/components/featured/Featured.jsx
+++ src/app/components/featured/Featured.jsx
@@ -1,6 +1,32 @@
 import React from "react";
 import styles from "./featured.module.css";
+import Image from "next/image";
 
 export const Featured = () => {
-  return <div className={styles.container}>Navbar</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey there.</b> Welcome to my merge of communication and tech,{" "}
+        <b>enjoy!</b>
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </h1>
+          <p className={styles.postDescription}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
 };
