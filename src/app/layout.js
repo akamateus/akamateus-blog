5cbd11dc3230d6ca079fd679c9ff8542b3f270b0
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-09-19 13:43:26 +0200

    added ThemeContext on layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { ThemeContextProvider } from "@/context/ThemeContext";
 import { Footer } from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
@@ -14,13 +15,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="container">
-          <div className="wrapper">
-            <Navbar />
-            {children}
-            <Footer />
+        <ThemeContextProvider>
+          <div className="container">
+            <div className="wrapper">
+              <Navbar />
+              {children}
+              <Footer />
+            </div>
           </div>
-        </div>
+        </ThemeContextProvider>
       </body>
     </html>
   );
