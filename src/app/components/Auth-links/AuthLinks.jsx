d91c0619d4764389857d12162078dc8f172efc36
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-09-19 17:04:32 +0200

    did the responsive design for navbar

--- src/app/components/Auth-links/AuthLinks.jsx
+++ src/app/components/Auth-links/AuthLinks.jsx
@@ -1,7 +1,10 @@
+"use client";
 import Link from "next/link";
 import styles from "./authLinks.module.css";
+import { useState } from "react";
 
 const AuthLinks = () => {
+  const [open, setOpen] = useState(false);
   //temporary
   const status = "notAuthenticated";
   return (
@@ -11,9 +14,14 @@ const AuthLinks = () => {
       ) : (
         <>
           <Link href="/write">Write</Link>
-          <span>Logout</span>
+          <span className={styles.link}>Logout</span>
         </>
       )}
+      <div className={styles.burger}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
     </>
   );
 };
