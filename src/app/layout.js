a25710895bd8e9e0338ed1ea3e054d3c8b0429dc
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-09-19 13:56:30 +0200

    added new font

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,19 +2,19 @@ import { ThemeContextProvider } from "@/context/ThemeContext";
 import { Footer } from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const pjs = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "akamateus",
+  title: "akaMateus",
   description: "Tech blog for juniors",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={pjs.className}>
         <ThemeContextProvider>
           <div className="container">
             <div className="wrapper">
