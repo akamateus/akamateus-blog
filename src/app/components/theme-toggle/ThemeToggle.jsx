32019bd1ffd6471adf1663c0eb6b5d405c02fbb3
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-09-19 13:21:10 +0200

    added toggle with css style

--- src/app/components/theme-toggle/ThemeToggle.jsx
+++ src/app/components/theme-toggle/ThemeToggle.jsx
@@ -1,7 +1,17 @@
 import styles from "./themeToggle.module.css";
 
+import { RiMoonClearLine } from "react-icons/ri";
+
+import { RiSunFoggyLine } from "react-icons/ri";
+
 const ThemeToggle = () => {
-  return <div className={styles.container}>ThemeToggle</div>;
+  return (
+    <div className={styles.container}>
+      <RiMoonClearLine width={14} height={14} />
+      <div className={styles.ball}></div>
+      <RiSunFoggyLine width={14} height={14} />
+    </div>
+  );
 };
 
 export default ThemeToggle;
