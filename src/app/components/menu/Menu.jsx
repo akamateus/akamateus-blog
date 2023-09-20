c8bc9c789c31693d4b9543e9b01c3b39fc282062
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-09-20 18:41:42 +0200

    .

--- src/app/components/menu/Menu.jsx
+++ src/app/components/menu/Menu.jsx
@@ -79,6 +79,81 @@ export const Menu = () => {
           </div>
         </Link>
       </div>
+
+      {/* Another section 🔦🔦🔦🔦🔦*/}
+      <h2 className={styles.subtitle}>{"Matt's Choice"}</h2>
+      <h1 className={styles.title}>Kinda Popular</h1>
+      <div className={styles.items}>
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image className={styles.image} src="/p1.jpeg" alt="" fill />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.travel}`}>
+              Travel
+            </span>
+            <h3 className={styles.postTitle}>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Matt Oliver</span>
+              <span className={styles.date}> - 10.12.2023</span>
+            </div>
+          </div>
+        </Link>
+
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image className={styles.image} src="/p1.jpeg" alt="" fill />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.culture}`}>
+              Culture
+            </span>
+            <h3 className={styles.postTitle}>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Matt Oliver</span>
+              <span className={styles.date}> - 10.12.2023</span>
+            </div>
+          </div>
+        </Link>
+
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image className={styles.image} src="/p1.jpeg" alt="" fill />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.food}`}>Food</span>
+            <h3 className={styles.postTitle}>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Matt Oliver</span>
+              <span className={styles.date}> - 10.12.2023</span>
+            </div>
+          </div>
+        </Link>
+
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image className={styles.image} src="/p1.jpeg" alt="" fill />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.fashion}`}>
+              Fashion
+            </span>
+            <h3 className={styles.postTitle}>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Matt Oliver</span>
+              <span className={styles.date}> - 10.12.2023</span>
+            </div>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
