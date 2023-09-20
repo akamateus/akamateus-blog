eec9312c4bcd36e57685eb9d068c57897705f3ec
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-09-20 11:47:59 +0200

    added css for side menu

--- src/app/components/menu/Menu.jsx
+++ src/app/components/menu/Menu.jsx
@@ -6,23 +6,23 @@ import Image from "next/image";
 export const Menu = () => {
   return (
     <div className={styles.container}>
-      <h2>{"Best content"}</h2>
-      <h1>Most Popular</h1>
+      <h2 className={styles.subtitle}>{"Best content"}</h2>
+      <h1 className={styles.title}>Most Popular</h1>
       <div className={styles.items}>
         <Link href="/" className={styles.item}>
           <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+            <Image className={styles.image} src="/p1.jpeg" alt="" fill />
           </div>
           <div className={styles.textContainer}>
             <span className={`${styles.category} ${styles.travel}`}>
               Travel
             </span>
-            <h3>
+            <h3 className={styles.postTitle}>
               Sed ut perspiciatis unde omnis iste natus error sit voluptatem.
             </h3>
             <div className={styles.detail}>
               <span className={styles.username}>Matt Oliver</span>
-              <span className={styles.date}>10.12.2023</span>
+              <span className={styles.date}> - 10.12.2023</span>
             </div>
           </div>
         </Link>
