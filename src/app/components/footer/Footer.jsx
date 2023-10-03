b1563d1fde2d9451cf76efa20d55cc375ed07bfc
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-10-03 05:33:29 +0200

    added footer

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -2,5 +2,10 @@ import React from "react";
 import styles from "./footer.module.css";
 
 export const Footer = () => {
-  return <div className={styles.container}>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.container}></div>
+      <div className={styles.links}></div>
+    </div>
+  );
 };
