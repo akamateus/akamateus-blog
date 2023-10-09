c0fe50901875541c86f9c60cea92e52a22d4b2c5
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-10-09 16:58:12 +0200

    added css style for single page

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -8,25 +8,51 @@ const SinglePage = () => {
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <div className={styles.textContainer}>
-          <h1> loren papa dhdu wubk caca pipuro mamagito ainda fhsu.</h1>
+          <h1 className={styles.title}>
+            {" "}
+            loren papa dhdu wubk caca pipuro mamagito ainda fhsu.
+          </h1>
           <div className={styles.user}>
-            <div className={styles.imageContainer}>
-              <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+            <div className={styles.userImageContainer}>
+              <Image src="/p1.jpeg" alt="" fill className={styles.avatar} />
             </div>
             <div className={styles.userTextContainer}>
-              <span className={styles.userName}> John Doe</span>
+              <span className={styles.username}> John Doe</span>
               <span className={styles.date}> 01.01.2023</span>
             </div>
           </div>
         </div>
 
         <div className={styles.imageContainer}>
-          <Image src="" alt="" fill className={styles.image} />
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
         </div>
       </div>
 
       <div className={styles.content}>
-        x<div className={styles.post}></div>
+        <div className={styles.post}>
+          <div className={styles.description}>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.
+            </p>
+            <h2> Loren blabs boren cudun</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.
+            </p>
+          </div>
+        </div>
         <Menu />
       </div>
     </div>
