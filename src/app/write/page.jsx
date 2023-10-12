439fd780132f7c2d5e311d630cffac362fd80f98
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-10-12 15:45:20 +0200

    added style css for write page

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -2,6 +2,12 @@
 import Image from "next/image";
 import styles from "./writePage.module.css";
 import { useState } from "react";
+import { HiOutlineSquaresPlus } from "react-icons/hi2";
+import {
+  HiOutlinePhotograph,
+  HiOutlineExternalLink,
+  HiOutlineVideoCamera,
+} from "react-icons/hi";
 
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.bubble.css";
@@ -12,32 +18,39 @@ const WritePage = () => {
 
   return (
     <div className={styles.container}>
-      <input type="text" placeholder="Title" />
+      <input type="text" placeholder="Title" className={styles.input} />
       <div className={styles.editor}>
         <button className={styles.button} onClick={() => setOpen(!open)}>
-          <Image src="/plus.png" alt="" height={16} width={16} />
+          <HiOutlineSquaresPlus alt="" height={16} width={16} />
         </button>
 
         {open && (
           <div className={styles.add}>
             <button className={styles.addButton}>
-              <Image src="/image.png" alt="" height={16} width={16} />
+              <HiOutlinePhotograph alt="" height={16} width={16} />
             </button>
             <button className={styles.addButton}>
-              <Image src="/external.png" alt="" height={16} width={16} />
+              <HiOutlineExternalLink alt="" height={16} width={16} />
             </button>
             <button className={styles.addButton}>
-              <Image src="/video.png" alt="" height={16} width={16} />
+              <HiOutlineVideoCamera
+                src="/video.png"
+                alt=""
+                height={16}
+                width={16}
+              />
             </button>
           </div>
         )}
         <ReactQuill
+          className={styles.textArea}
           theme="bubble"
           value={value}
           onChange={setValue}
           placeholder="You write here... ✍🏽"
         />
       </div>
+      <button className={styles.postButton}>Post</button>
     </div>
   );
 };
