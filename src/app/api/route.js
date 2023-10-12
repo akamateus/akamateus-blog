540d7c0261ef7d23d6b18d91743d3aa084c91d19
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-10-12 23:47:00 +0200

    added utils

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,16 +1,5 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
 
-export default NextAuth({
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    GoogleProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-  ],
-});
+import { authOptions } from "@/utils/auth";
+
+export default NextAuth(authOptions);
