8a7ba57686a4dca006d3dc8e14080443cda522fe
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-10-13 19:51:10 +0200

    added mongo but not working

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,7 +1,11 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import prisma from "./connect";
+import { Prisma } from "@prisma/client";
 
 export const authOptions = {
+  adapter: PrismaAdapter(Prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
