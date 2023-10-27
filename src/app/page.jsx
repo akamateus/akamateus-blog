7777b5ee308af122b98ad0a053be954f72406aec
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-10-27 15:32:35 +0200

    created schema for users, set routes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,18 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
 import { Featured } from "./components/featured/Featured";
-import { CategoryList } from "./components/category-list/CategoryList";
+import CategoryList from "./components/category-list/CategoryList";
 import { CardList } from "./components/card-list/CardList";
 import { Menu } from "./components/menu/Menu";
 
-export default function Home() {
+export default function Home({ searchParams }) {
+  const page = parseInt(searchParams.page) || 1;
   return (
     <div className={styles.container}>
       <Featured />
       <CategoryList />
       <div className={styles.content}>
-        <CardList />
+        <CardList page={page} />
         <Menu />
       </div>
     </div>
